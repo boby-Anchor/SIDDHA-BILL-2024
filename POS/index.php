6dc89a45188c556ce23e676faad5bd1aa2844601
pavandesilva <pavanmanoj24@gmail.com>
2024-10-04 08:40:37 +0530

    04-10-2024

--- POS/index.php
+++ POS/index.php
@@ -1,32 +1,27 @@
 <?php
 session_start();
+
 if (isset($_SESSION['store_id'])) {
 
   $userLoginData = $_SESSION['store_id'];
+  $redirects = [
+    '1' => 'super_admin_dashboard.php',
+    '2' => 'all-today-cashiers.php',
+    '3' => 'manage-shop.php',
+    '4' => 'stock.php',
+    '5' => 'pos.php',
+    '6' => 'ac.php'
+  ];
 
   foreach ($userLoginData as $userData) {
-
     $userRoleId = $userData['user_role_id'];
 
-    if ($userRoleId == '1') {
-      echo "<script>window.location='super_admin_dashboard.php';</script>";
-      exit; // Exit the loop after redirection
-    } elseif ($userRoleId == '2') {
-      echo "<script>window.location='all-today-cashiers.php';</script>";
-      exit; // Exit the loop after redirection
-    } elseif ($userRoleId == '3') {
-      echo "<script>window.location='manage-shop.php';</script>";
-      exit; // Exit the loop after redirection
-    } elseif ($userRoleId == '4') {
-      echo "<script>window.location='add-stock.php';</script>";
-      exit; // Exit the loop after redirection
-    } elseif ($userRoleId == '5') {
-      echo "<script>window.location='pos.php';</script>";
-      exit; // Exit the loop after redirection
-    } elseif ($userRoleId == '6') {
-      echo "<script>window.location='ac.php';</script>";
-      exit; // Exit the loop after redirection
+    if (isset($redirects[$userRoleId])) {
+      echo "<script>window.location='" . $redirects[$userRoleId] . "';</script>";
+      exit;
+    } else {
+      echo "<script>alert('Unknown role!');</script>";
+      exit;
     }
-    
   }
 }
