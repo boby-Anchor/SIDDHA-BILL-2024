16d9f6f818a7313f29e7415d30420f214cd922df
pavandesilva <pavanmanoj24@gmail.com>
2025-04-29 10:26:21 +0530

    Reapply "Alert updates and minor fixes"
    This reverts commit e4308fae1e1ddb7ab26ed5a3764620890aa3e1dd.

--- POS/dist/js/po.js
+++ POS/dist/js/po.js
@@ -213,19 +213,12 @@ function checkNetTotal() {
 function calculateSubTotal() {
   var productsAllTotal = 0;
   $(".barcodeResults tbody tr").each(function () {
+    var product_name = $(this).find("#product_name").text();
     var product_cost = $(this).find("#product_price").text();
     var product_qty = $(this).find("#qty").val();
 
     if (product_qty === "" || product_qty === "0") {
-      Swal.mixin({
-        toast: true,
-        position: "top-end",
-        showConfirmButton: false,
-        timer: 3000,
-      }).fire({
-        icon: "error",
-        title: "Error: Product Quantity!",
-      });
+      ErrorMessageDisplay(product_name + "invalid Quantity!");
       $("#checkoutBtn").removeAttr("data-toggle data-target");
     } else {
       var productTotal = product_cost * product_qty;
@@ -357,6 +350,11 @@ function checkout() {
   var discount_percentage = $("#discountPercentage").val();
   var net_total = $("#netTotal").text().replace(/,/g, "");
 
+  if (po_shop_id == 0) {
+    ErrorMessageDisplay("Shop එක select කරන්නේ නැද්ද?");
+    return;
+  }
+
   var poArray = [];
   var inArray = [];
 
@@ -405,15 +403,7 @@ function checkout() {
       products: JSON.stringify(poArray),
     },
     success: function (response) {
-      Swal.mixin({
-        toast: true,
-        position: "top-end",
-        showConfirmButton: false,
-        timer: 3000,
-      }).fire({
-        icon: "success",
-        title: "Success: Order Placed Successfully!",
-      });
+      SuccessMessageDisplay("Success: Order Placed Successfully!");
       $(".confirmPObtn").prop("disabled", false);
 
       $.ajax({
@@ -424,32 +414,15 @@ function checkout() {
         },
         success: function (response) {
           document.getElementById("printInvoiceData").innerHTML = response;
-          // console.log(response);
           printInvoice();
         },
         error: function (xhr, status, error) {
-          Swal.mixin({
-            toast: true,
-            position: "top-end",
-            showConfirmButton: false,
-            timer: 3000,
-          }).fire({
-            icon: "error",
-            title: "Bill print error!",
-          });
+          ErrorMessageDisplay("Bill print error!");
         },
       });
     },
     error: function (xhr, status, error) {
-      Swal.mixin({
-        toast: true,
-        position: "top-end",
-        showConfirmButton: false,
-        timer: 3000,
-      }).fire({
-        icon: "error",
-        title: "Error: Something went wrong!",
-      });
+      ErrorMessageDisplay("Something went wrong!");
     },
   });
 }
