293e1548f8eb3c21939e45ddf36b65d4d3dd0496
pavandesilva <pavanmanoj24@gmail.com>
2025-05-10 10:33:53 +0530

    edit stock save

--- POS/actions/edit-stock_new_action.php
+++ POS/actions/edit-stock_new_action.php
@@ -32,7 +32,7 @@ if (isset($_POST['products'])) {
 
     $poArray = json_decode($_POST['products'], true);
 
-    $errorOccured = false;
+    $errorOccurred = false;
     $notificationMessage = "";
 
     if (is_array($poArray) && !empty($poArray)) {
@@ -57,26 +57,45 @@ if (isset($_POST['products'])) {
 
                     // Insert into stock
 
-                    try {
-                        $conn->query("INSERT INTO stock2 (stock_item_code, stock_item_name, stock_item_qty, stock_mu_qty, item_s_price, unit_s_price, stock_shop_id)
+                    $stock_result = $conn->query("SELECT * FROM  stock2 WHERE stock_item_code = '$product_code'
+                    AND item_s_price = '$item_price' AND stock_shop_id = '$shop_id'");
+
+                    if ($stock_result && $stock_result->num_rows > 0) {
+                        try {
+                            $stock_data = $stock_result->fetch_assoc();
+                            $updated_qty = floatval($stock_data["stock_item_qty"]) + floatval($product_qty);
+                            $updated_minimum_qty = floatval($stock_data["stock_mu_qty"]) + floatval($minimum_qty);
+
+                            $conn->query("UPDATE stock2 SET stock_item_qty = '$updated_qty', stock_mu_qty = '$updated_minimum_qty', unit_s_price = '$unit_s_price'
+                            WHERE stock_item_code = '$product_code' AND item_s_price='$item_price' AND stock_shop_id = '$shop_id'");
+                            $notificationMessage .= "Success " . $product_name . " updated.\n";
+                        } catch (Exception $exception) {
+                            $errorOccurred = true;
+                            printErrorLog($exception->getMessage());
+                            $notificationMessage .= "Failed " . $product_name . " stock UPDATE. Check error_log.\n";
+                        }
+                    } else {
+                        try {
+                            $conn->query("INSERT INTO stock2 (stock_item_code, stock_item_name, stock_item_qty, stock_mu_qty, item_s_price, unit_s_price, stock_shop_id)
                                 VALUES ('$product_code','$product_name','$product_qty','$minimum_qty', '$item_price', '$unit_s_price', '$shop_id')");
-                        $notificationMessage .= "Success " . $product_name . " Inserted new stock.\n";
-                    } catch (Exception $exception) {
-                        $errorOccured = true;
-                        $error_message = "Error: " . $conn->error . " " . $exception->getMessage() . " " . $grn_number . "Stock data insert failed of Code-" . $product_code . ", Name-"
-                            . $product_name . " qty-" . $product_qty . " min qty-" . $minimum_qty . ' discount-' . $item_discount . ' unit price-' . $unit_s_price . ' total cost-' . $total_cost . ' free qty-' . $free_qty . "\n";
-                        printErrorLog($error_message);
-                        $notificationMessage .= "Failed " . $product_name . " stock INSERT. Check error_log.\n";
+                            $notificationMessage .= "Success " . $product_name . " Inserted new stock.\n";
+                        } catch (Exception $exception) {
+                            $errorOccurred = true;
+                            $error_message = "Error: " . $conn->error . " " . $exception->getMessage() . " " . $grn_number . "Stock data insert failed of Code-" . $product_code . ", Name-"
+                                . $product_name . " qty-" . $product_qty . " min qty-" . $minimum_qty . ' discount-' . $item_discount . ' unit price-' . $unit_s_price . ' total cost-' . $total_cost . ' free qty-' . $free_qty . "\n";
+                            printErrorLog($error_message);
+                            $notificationMessage .= "Failed " . $product_name . " stock INSERT. Check error_log.\n";
+                        }
                     }
                 }
             }
         } catch (Exception $exception) {
-            $errorOccured = true;
+            $errorOccurred = true;
             $error_message = "ERROR: " . $exception->getMessage() . "\n";
             printErrorLog($error_message);
             $notificationMessage .= $exception->getMessage();
         } finally {
-            if ($errorOccured) {
+            if ($errorOccurred) {
                 echo json_encode(array(
                     'status' => 'error',
                     'message' => $error_message
