41e854e99977d83809d87ed036b33397988cea83
pavandesilva <pavanmanoj24@gmail.com>
2025-05-15 17:47:34 +0530

    PO confirmation and js

--- POS/dist/js/add-purchase.js
+++ POS/dist/js/add-purchase.js
@@ -1,6 +1,7 @@
+
 function select_suplier(sup_id) {
   var req = new XMLHttpRequest();
-  req.onreadystatechange = function () {
+  req.onreadystatechange = function() {
     if (req.readyState == 4 && req.status == 200) {
       var response = req.response;
       document.getElementById("filterBySupTable").innerHTML = response;
@@ -18,7 +19,7 @@ function addNewUnit() {
   var newUnit = document.getElementById("newUnit").value;
 
   var req = new XMLHttpRequest();
-  req.onreadystatechange = function () {
+  req.onreadystatechange = function() {
     if (req.readyState == 4 && req.status == 200) {
       var response = req.responseText;
       var iconType;
@@ -47,83 +48,130 @@ function addNewUnit() {
 
 // ==============================================================================
 
-{
-  $(document).on("click", ".proceed-order", function () {
-    var poArray = [];
 
-    $(".addedProTable tbody tr")
-      .slice(1)
-      .each(function () {
-        var product_code = $(this).find("th").text();
-        var product_name = $(this).find("#addproduct_name").text();
-        var product_cost = $(this).find("#addproduct_cost").text();
-        var product_qty = $(this).find("input").val();
-        var unit = $(this).find("select[name='quantity_unit']").val();
+$(document).on("click", ".proceed-order", function() {
 
-        if (product_qty === "" || product_qty === "0") {
-          Swal.mixin({
-            toast: true,
-            position: "top-end",
-            showConfirmButton: false,
-            timer: 3000,
-          }).fire({
-            icon: "error",
-            title: "Error: Product Quantity!",
-          });
+  if (!$("#order_date").val()) {
+    ErrorMessageDisplay("Select order Date");
+  }
+  var poArray = [];
 
-          $("#proceedOrderBtn").removeAttr("data-toggle data-target");
-        } else if (unit === "0") {
-          Swal.mixin({
-            toast: true,
-            position: "top-end",
-            showConfirmButton: false,
-            timer: 3000,
-          }).fire({
-            icon: "error",
-            title: "Error: Product Unit!",
-          });
+  var markup = "";
 
-          $("#proceedOrderBtn").removeAttr("data-toggle data-target");
-        } else {
-          var productData = {
-            product_code: product_code,
-            product_name: product_name,
-            product_cost: product_cost,
-            product_qty: product_qty,
-            unit: unit,
-          };
-          poArray.push(productData);
+  $(".addedProTable tbody tr").each(function() {
+    var product_code = $(this).find("#product_code").text();
+    var product_name = $(this).find("#product_name").text();
+    var product_brand = $(this).find("#product_brand").text();
+    var product_qty = $(this).find("#product_qty").val();
+    var product_unit = $(this).find("#product_unit").text();
 
-          $.ajax({
-            url: "poConfirmation.php",
-            method: "POST",
-            data: {
-              products: poArray,
-            },
-            success: function (response) {
-              document.getElementById("orderConfirmationTableBody").innerHTML =
-                response;
-            },
-            error: function (xhr, status, error) {
-              console.error(xhr.responseText);
-            },
-          });
+    if (product_qty === "" || product_qty === "0") {
+      ErrorMessageDisplay(product_name + "Invalid qty");
+      $("#proceedOrderBtn").removeAttr("data-toggle data-target");
+    } else {
+      var productData = {
+        product_code: product_code,
+        product_name: product_name,
+        product_brand: product_brand,
+        product_qty: product_qty,
+        product_unit: product_unit,
+      };
+      console.log(productData);
 
-          $("#proceedOrderBtn").attr({
-            "data-toggle": "modal",
-            "data-target": "#confirmPO",
-          });
-        }
+      markup +=
+        "<tr class=''>" +
+        "<th class='product_code'>" + product_code + "</th>" +
+        "<td class='product_name'>" + product_name + "</td>" +
+        "<td class='product_brand'>" + product_brand + "</td>" +
+        "<td class'product_unit'>" + product_unit + "</td>" +
+        "<td class='product_qty'>" + product_qty + " </td>" +
+        "<td class=''></td>" +
+        "</tr>";
+
+      $(".orderConfirmationTable tbody").html(markup);
+
+      $(".po_btn").toggleClass("d-none", $(".addedProTable tbody tr").length === 0);
+      $(".po_btn").toggleClass("d-flex", $(".addedProTable tbody tr").length > 0);
+
+      poArray.push(productData);
+
+      // $.ajax({
+      //   url: "poConfirmation.php",
+      //   method: "POST",
+      //   data: {
+      //     products: poArray,
+      //   },
+      //   success: function (response) {
+      //     document.getElementById("orderConfirmationTableBody").innerHTML =
+      //       response;
+      //   },
+      //   error: function (xhr, status, error) {
+      //     console.error(xhr.responseText);
+      //   },
+      // });
+
+      $("#proceedOrderBtn").attr({
+        "data-toggle": "modal",
+        "data-target": "#confirmPO",
       });
-
-    console.log(poArray);
+    }
   });
-}
+});
+
+
+$(document).on("click", ".add-btn", function() {
+  var product_code = $(this).closest("tr").find("#product_code").text();
+  var product_name = $(this).closest("tr").find("#product_name").text();
+  var product_brand = $(this).closest("tr").find("#product_brand").text();
+  var product_volume = $(this).closest("tr").find("#product_volume").text();
+  var product_unit = $(this).closest("tr").find("#product_unit").text();
+
+  var exists = false;
+  $(".addedProTable tbody tr").each(function() {
+    if ($(this).find("#product_code").text() === product_code) {
+      exists = true;
+      return false;
+    }
+  });
+
+  if (!exists) {
+    var markup =
+      "<tr class='row'>" +
+      "<th class='col-1' id='product_code'>" + product_code + "</th>" +
+      "<td class='col-3' id='product_name'>" + product_name + "</td>" +
+      "<td class='col-2' id='product_brand'>" + product_brand + "</td>" +
+      "<td class='col-1 text-center' id='product_unit'>" + product_volume + product_unit + "</td>" +
+      "<td class='col-2' colspan=2> <input type='number' class='form-control bg-dark' id='product_qty' oninput='this.value = this.value.replace(/[^0-9]/g, '')' /> </td>" +
+      "<td class='col-1'></td>" +
+      "<td class='col-1 text-right'><i class='fa fa-trash-o cus-delete'></i></td>" +
+      "</tr>";
+
+    $(".addedProTable tbody").append(markup);
+
+    $(".po_btn").toggleClass("d-none", $(".addedProTable tbody tr").length === 0);
+    $(".po_btn").toggleClass("d-flex", $(".addedProTable tbody tr").length > 0);
+
+  } else {
+    ErrorMessageDisplay("Product already exists in the list!");
+  }
+});
+
+$(document).on("click", ".cus-delete", function() {
+  $(this).closest("tr").remove();
+  $("#proceedOrderBtn").removeAttr("data-toggle data-target");
+  $(".po_btn").toggleClass("d-none", $(".addedProTable tbody tr").length === 1);
+  $(".po_btn").toggleClass("d-flex", $(".addedProTable tbody tr").length > 1);
+});
+
+$('#myModal').on('shown.bs.modal', function() {
+  $('#myInput').trigger('focus')
+})
+
 
 // ==============================================================================
 
 function filterBySearch(searchTxt) {
- // alert("in search");
+  // alert("in search");
   //console.log("in search");
   var bnInput = document.getElementById("bnInput").value;
   var pcInput = document.getElementById("pcInput").value;
@@ -156,13 +204,13 @@ function filterBySearch(searchTxt) {
   form.append("searchBy", searchBy);
 
   var req = new XMLHttpRequest();
-  req.onreadystatechange = function () {
+  req.onreadystatechange = function() {
     if (req.readyState == 4 && req.status == 200) {
       var response = req.responseText;
       document.getElementById("filterBySupTable").innerHTML = response;
     }
   };
-req.open("POST", "filterBySearch-PO.php", true); 
+  req.open("POST", "filterBySearch-PO.php", true);
   req.send(form);
 }
 
@@ -171,7 +219,7 @@ req.open("POST", "filterBySearch-PO.php", true);
 function refreshOptions(selectedItem) {
   if (selectedItem == "0") {
     var req = new XMLHttpRequest();
-    req.onreadystatechange = function () {
+    req.onreadystatechange = function() {
       if (req.readyState == 4 && req.status == 200) {
         var response = req.responseText;
         document.getElementById("unitselectordiv").innerHTML = response;
@@ -181,3 +229,58 @@ function refreshOptions(selectedItem) {
     req.send();
   }
 }
+
+$(document).off("click", ".confirmPObtn").on("click", ".confirmPObtn", function() {
+
+  $(".confirmPObtn").prop('disabled', true);
+
+  var deliveryDate = $("#order_date").val()
+  var orderNumber = document.getElementById("orderNumber").innerText;
+  var orderDate = document.getElementById("orderDate").innerText;
+  var orderTime = document.getElementById("orderTime").innerText;
+  $(this).prop('disabled', true);
+
+  var poArray = [];
+  var poBillData = {
+    orderNumber: orderNumber,
+    orderDate: orderDate,
+    orderTime: orderTime,
+    deliveryDate: deliveryDate,
+  }
+
+  $("#orderConfirmationTableBody tr").each(function() {
+    var product_code = $(this).find(".product_code").text();
+    var product_name = $(this).find(".product_name").text();
+    var product_brand = $(this).find(".product_brand").text();
+    var product_qty = $(this).find(".product_qty").text();
+    var qty_unit = $(this).find(".qty_unit").text();
+
+    var productData = {
+      product_code: product_code,
+      product_name: product_name,
+      product_brand: product_brand,
+      product_qty: product_qty,
+    };
+    poArray.push(productData);
+  });
+
+  $.ajax({
+    url: "poConfirmationInsert.php",
+    method: "POST",
+    data: {
+      products: JSON.stringify(poArray),
+      poBillData: JSON.stringify(poBillData),
+    },
+    success: function(response) {
+      SuccessMessageDisplay(response);
+      setTimeout(function() {
+        location.reload();
+      }, 3000);
+    },
+    error: function(xhr, status, error) {
+      console.error(xhr.responseText);
+      ErrorMessageDisplay("Order Failed !");
+      $(".confirmPObtn").prop('disabled', false);
+    },
+  });
+});
--- POS/poConfirmationInsert.php
+++ POS/poConfirmationInsert.php
@@ -1,73 +1,73 @@
 <?php
 session_start();
 include('config/db.php');
+$poBillData = json_decode($_POST['poBillData'], true);
 $poArray = json_decode($_POST['products'], true);
 $productsAllTotal = 0;
+$message = "";
 
-echo "Length of poArray: " . count($poArray) . "<br>";
+try {
+    function printErrorLog($error_message)
+    {
+        $error_log_path = "error_log.txt";
+        file_put_contents($error_log_path, $error_message, FILE_APPEND);
+    }
+} catch (Exception $exception) {
+    echo json_encode(array(
+        'status' => 'error',
+        'message' => 'path Fatal Error. Contact IT Department',
+    ));
+    exit();
+}
 
-if (isset($_SESSION['store_id'])) {
+try {
 
+    if (isset($_SESSION['store_id'])) {
 
+        $userLoginData = $_SESSION['store_id'];
 
-    $userLoginData = $_SESSION['store_id'];
+        foreach ($userLoginData as $userData) {
 
-    foreach ($userLoginData as $userData) {
+            $shop_id = $userData['shop_id'];
 
+            if (is_array($poArray) && !empty($poArray)) {
 
+                $orderNumber = $poBillData['orderNumber'];
+                $orderDate = $poBillData['orderDate'];
+                $orderTime = $poBillData['orderTime'];
+                $deliveryDate = $poBillData['deliveryDate'];
 
-        $shop_id = $userData['shop_id'];
+                $orderDateTime = $orderDate . ' ' . $orderTime;
 
-        if (is_array($poArray) && !empty($poArray)) {
+                foreach ($poArray as $product) {
 
-            foreach ($poArray as $product) {
+                    $product_code = $product['product_code'];
+                    $product_name = $product['product_name'];
+                    $product_brand = $product['product_brand'];
+                    $product_qty = intval($product['product_qty']);
 
-
-
-                $product_code = $product['product_code'];
-                $product_name = $product['product_name'];
-                $product_brand = $product['product_brand'];
-                $product_cost = floatval($product['product_cost']);
-                $product_qty = intval($product['product_qty']);
-                $unit = $product['qty_unit'];
-                $orderNumber = $product["orderNumber"];
-                $orderDate = $product["orderDate"];
-                $orderTime = $product["orderTime"];
-
-                // $conn->query("INSERT INTO test (c1,c2)VALUES ('$product_code,'$product_name')");
-
-
-                if (
-                    !empty($product['product_code']) && !empty($product['product_name'])   // && !empty($product['product_brand']) 
-                    && !empty($product['product_cost']) && !empty($product['product_qty']) && !empty($product['qty_unit'])
-                ) {
-
-
-                    $orderDateTime = $product["orderDate"] . ' ' . $product["orderTime"];
-
-
-                    $productTotal = $product_cost * $product_qty;
-
-                    $productsAllTotal += $productTotal;
-
-
-                    $conn->query("INSERT INTO hub_order (HO_number, HO_item, HO_brand,HO_qty, hub_order_unit, HO_price, HO_total,HO_shopId) 
-                    VALUES ('$orderNumber', '$product_code', '$product_brand','$product_qty', '$unit', '$product_cost', '$productTotal','$shop_id')");
-                    echo "success";
-                } else {
-                    echo "Invalid product entry ";
+                    if (
+                        !empty($product['product_code']) && !empty($product['product_name'])
+                        && !empty($product['product_qty'])
+                    ) {
+                        $conn->query("INSERT INTO hub_order (HO_number, HO_item, HO_brand, HO_qty, hub_order_unit, HO_price, HO_total,HO_shopId) 
+                    VALUES ('$orderNumber', '$product_code', '$product_brand','$product_qty', '', '0', '0','$shop_id')");
+                    } else {
+                        echo "Invalid product entry ";
+                    }
                 }
+
+                $orderDateTimeObj = new DateTime($orderDateTime, new DateTimeZone('Asia/Colombo'));
+                $dateTimeFormatted = $orderDateTimeObj->format('Y-m-d H:i:s');
+
+                $conn->query("INSERT INTO hub_order_details (hub_order_number,hub_order_subTotal,hub_order_status,hub_order_paymentType,hub_order_paymentStatus,order_date,delivery_date)
+            VALUES ('$orderNumber','0','1','1','1','$dateTimeFormatted','$deliveryDate')");
+            } else {
+                echo "No products found or invalid data received.";
             }
-
-
-            $orderDateTimeObj = new DateTime($orderDateTime, new DateTimeZone('Asia/Colombo'));
-            $dateTimeFormatted = $orderDateTimeObj->format('Y-m-d H:i:s');
-
-            $conn->query("INSERT INTO hub_order_details (hub_order_number,hub_order_subTotal,hub_order_status,hub_order_paymentType,hub_order_paymentStatus,HO_date)
-            VALUES ('$orderNumber','$productsAllTotal','1','1','1','$dateTimeFormatted')");
-        } else {
-
-            echo "No products found or invalid data received.";
         }
     }
+} catch (Exception $exception) {
+    printErrorLog($exception->getMessage() . $message);
+    echo ($exception->getMessage() . $message);
 }
