16bd99fc66cb5b2d23213f55b4cd7ca0cabef7ca
pavandesilva <pavanmanoj24@gmail.com>
2025-05-28 17:21:25 +0530

    Updates to manage PO

--- POS/manage-purchase.php
+++ POS/manage-purchase.php
@@ -65,21 +65,21 @@ if (!isset($_SESSION['store_id'])) {
                               <th class="adThText">Order From</th>
                               <th class="adThText">Items</th>
                               <th class="adThText">Order Placed Date</th>
-                              <th class="adThText">Total Price</th>
+                              <th class="adThText">Required by</th>
                               <th class="adThText">Action</th>
                             </tr>
                           </thead>
                           <tbody>
                             <?php
                             if ($shop_id == "1") {
-                              $hub_order_details_result = $conn->query("SELECT DISTINCT hub_order_details_id , hub_order_number , shopName , order_date , hub_order_subTotal , order_status , order_status_id
+                              $hub_order_details_result = $conn->query("SELECT DISTINCT hub_order_details_id , hub_order_number , shopName , order_date , hub_order_subTotal , order_status , order_status_id, delivery_date
                               FROM hub_order_details 
                               INNER JOIN hub_order ON hub_order.HO_number = hub_order_details.hub_order_number
                               INNER JOIN order_status ON order_status.order_status_id = hub_order_details.hub_order_status
                               INNER JOIN shop ON shop.shopId = hub_order.HO_shopId
                               ORDER BY `hub_order_details_id` DESC");
                             } else {
-                              $hub_order_details_result = $conn->query("SELECT DISTINCT hub_order_details_id , hub_order_number , shopName , order_date , hub_order_subTotal , order_status , order_status_id
+                              $hub_order_details_result = $conn->query("SELECT DISTINCT hub_order_details_id , hub_order_number , shopName , order_date , hub_order_subTotal , order_status , order_status_id, delivery_date
                               FROM hub_order_details 
                               INNER JOIN hub_order ON hub_order.HO_number = hub_order_details.hub_order_number
                               INNER JOIN order_status ON order_status.order_status_id = hub_order_details.hub_order_status
@@ -109,15 +109,26 @@ if (!isset($_SESSION['store_id'])) {
                                             <th scope="col">#</th>
                                             <th scope="col">Barcode</th>
                                             <th scope="col">Item Name</th>
+                                            <th scope="col">Volume</th>
+                                            <th scope="col">Brand</th>
                                             <th scope="col">Qty</th>
-                                            <th scope="col">Cost</th>
                                           </tr>
                                         </thead>
                                         <tbody>
                                           <?php
 
-                                          $poItems_result = $conn->query("SELECT * FROM hub_order 
-                                        INNER JOIN p_medicine ON p_medicine.code = hub_order.HO_item 
+                                          $poItems_result = $conn->query("SELECT hub_order.HO_item AS code,
+                                          p_medicine.name AS name,
+                                          p_brand.name AS brand, 
+                                          hub_order.HO_qty AS HO_qty,
+                                          hub_order.HO_price AS HO_price,
+                                          unit_category_variation.ucv_name AS volume,
+                                          medicine_unit.unit AS unit
+                                          FROM hub_order
+                                        INNER JOIN p_medicine ON p_medicine.code = hub_order.HO_item
+                                        INNER JOIN p_brand ON p_medicine.brand = p_brand.id
+                                        INNER JOIN unit_category_variation ON p_medicine.unit_variation = unit_category_variation.ucv_id
+                                        INNER JOIN medicine_unit ON unit_category_variation.p_unit_id = medicine_unit.id
                                         WHERE HO_number = '" . $hub_order_details_data['hub_order_number'] . "'  ");
 
                                           $rowId = 0;
@@ -130,19 +141,20 @@ if (!isset($_SESSION['store_id'])) {
                                               <th scope="row"><?= $rowId ?></th>
                                               <td><?= $poItems_data["code"] ?></td>
                                               <td><?= $poItems_data["name"] ?></td>
+                                              <td><?= $poItems_data["volume"] ?> <?= $poItems_data["unit"] ?></td>
+                                              <td><?= $poItems_data["brand"] ?></td>
                                               <td><?= $poItems_data["HO_qty"] ?></td>
-                                              <td><?= number_format($poItems_data["HO_price"], 0) ?></td>
                                             </tr>
                                           <?php
                                           }
                                           ?>
                                         </tbody>
-                                        <tfoot>
+                                        <!-- <tfoot>
                                           <tr>
                                             <td colspan="4" class="text-right"><strong>Total Cost:</strong></td>
-                                            <td><?php echo  number_format($billTotal, 0); ?></td>
+                                            <td><?php //echo  number_format($billTotal, 0); ?></td>
                                           </tr>
-                                        </tfoot>
+                                        </tfoot> -->
                                       </table>
                                       <button class="btn btn-warning" style="font-weight: bold; font-family: 'Source Sans Pro';" onclick="printTable('<?= $hub_order_details_data['hub_order_number'] ?>');">
                                         <i class="nav-icon fas fa-copy"></i> PRINT
@@ -151,7 +163,7 @@ if (!isset($_SESSION['store_id'])) {
                                     <!--Items list table end  -->
                                   </th>
                                   <th><?= $hub_order_details_data['order_date'] ?></th>
-                                  <th><?= number_format($hub_order_details_data["hub_order_subTotal"], 2) ?></th>
+                                  <th><?= $hub_order_details_data["delivery_date"] ?></th>
                                   <th>
                                     <?php
                                     if ($shop_id == "1") {
