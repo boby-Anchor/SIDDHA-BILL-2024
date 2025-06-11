8bd7a4763f0b3d0d20ff2991cac60f1a9f08f495
pavandesilva <pavanmanoj24@gmail.com>
2025-06-11 20:54:59 +0530

    purchase order status update back end validations

--- POS/actions/purchase_orders/updateOrderStatusProcess.php
+++ POS/actions/purchase_orders/updateOrderStatusProcess.php
@@ -1,8 +1,25 @@
 <?php
 include('../../config/db.php');
-$orderNumber = $_POST['orderNumber'];
-$status = $_POST['status'];
+try {
 
-$conn->query("UPDATE hub_order_details SET hub_order_status = '$status' WHERE hub_order_number = '$orderNumber'");
+    $orderNumber = $_POST['orderNumber'] ?? null;
+    $status = $_POST['status'] ?? null;
 
-// echo 'success';
+    if (!$orderNumber || !$status) {
+        throw new Exception('Missing required parameters.');
+    }
+
+    if ($conn->query("UPDATE hub_order_details SET hub_order_status = '$status' WHERE hub_order_number = '$orderNumber'")) {
+        echo json_encode(array(
+            'status' => 'success',
+            'message' => 'Order status updated!',
+        ));
+    } else {
+        throw new Exception('Status update failed!');
+    }
+} catch (Exception $exception) {
+    echo json_encode(array(
+        'status' => 'error',
+        'message' => $exception->getMessage(),
+    ));
+}
