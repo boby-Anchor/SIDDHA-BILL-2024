6754f58acfedcf9830ad872d4b62aa6c658a8728
Pavan-De-Silva <pavanmanoj24@gmail.com>
2025-07-07 14:28:31 +0530

    Update po.php

--- POS/po.php
+++ POS/po.php
@@ -167,48 +167,57 @@ if (!isset($_SESSION['store_id'])) {
                                     onkeyup="searchProducts();" placeholder="Search...">
                                 <div class="row" id="productGrid" class="productGrid">
                                     <?php
-                                    if (isset($_SESSION['store_id'])) {
+                                    // if (isset($_SESSION['store_id'])) {
 
-                                        $userLoginData = $_SESSION['store_id'];
+                                    //     $userLoginData = $_SESSION['store_id'];
 
-                                        // foreach ($userLoginData as $userData) {
-                                        //     $shop_id = $userData['shop_id'];
+                                    //     // foreach ($userLoginData as $userData) {
+                                    //     //     $shop_id = $userData['shop_id'];
 
-                                        $cm = runQuery("SELECT stock2.*, p_brand.name AS bName, p_medicine.code AS code, p_medicine.name AS name,
-                                            medicine_unit.unit AS unit , unit_category_variation.ucv_name
-                                            FROM stock2
-                                            INNER JOIN p_medicine ON p_medicine.code = stock2.stock_item_code
-                                            INNER JOIN p_brand ON p_brand.id = p_medicine.brand
-                                            INNER JOIN medicine_unit ON medicine_unit.id = p_medicine.medicine_unit_id
-                                            INNER JOIN unit_category_variation ON unit_category_variation.ucv_id = p_medicine.unit_variation
-                                            WHERE stock2.stock_shop_id = '$shop_id' AND stock2.stock_item_qty > 0 ORDER BY p_medicine.name ASC");
+                                    //     $cm = runQuery("SELECT stock2.*, p_brand.name AS bName, p_medicine.code AS code, p_medicine.name AS name,
+                                    //         medicine_unit.unit AS unit , unit_category_variation.ucv_name
+                                    //         FROM stock2
+                                    //         INNER JOIN p_medicine ON p_medicine.code = stock2.stock_item_code
+                                    //         INNER JOIN p_brand ON p_brand.id = p_medicine.brand
+                                    //         INNER JOIN medicine_unit ON medicine_unit.id = p_medicine.medicine_unit_id
+                                    //         INNER JOIN unit_category_variation ON unit_category_variation.ucv_id = p_medicine.unit_variation
+                                    //         WHERE stock2.stock_shop_id = '$shop_id' AND stock2.stock_item_qty > 0 ORDER BY p_medicine.name ASC");
 
-                                        if (!empty($cm)) {
-                                            foreach ($cm as $v) {
+                                    //     if (!empty($cm)) {
+                                    //         foreach ($cm as $v) {
                                     ?>
-                                                <div class="col-md-4 col-sm-6 mt-3" onclick="getBarcode2('<?= $v['code']; ?>')">
+                                    <!-- <div class="col-md-4 col-sm-6 mt-3" onclick="getBarcode2('<?php // $v['code']; 
+                                                                                                    ?>')">
                                                     <div class="product-grid h-100">
                                                         <div class="product-content">
-                                                            <div class="name" style="color: #fff;"><?php echo $v['name']; ?> <br>
-                                                                <?= $v['code']; ?></div>
-                                                            <div class="name" style="color: #f67019;"><?php echo $v['bName']; ?>
+                                                            <div class="name" style="color: #fff;"><?php // echo $v['name']; 
+                                                                                                    ?> <br>
+                                                                <?php // $v['code']; 
+                                                                ?></div>
+                                                            <div class="name" style="color: #f67019;"><?php // echo $v['bName']; 
+                                                                                                        ?>
                                                             </div>
                                                             <div class="price" style="color: #3dce12;">I:- RS
-                                                                <?php echo $v['item_s_price']; ?>
+                                                                <?php //echo $v['item_s_price']; 
+                                                                ?>
                                                             </div>
                                                             <div class="price" style="color: #d8f13b;">U:- RS
-                                                                <?php echo $v['unit_s_price']; ?>
+                                                                <?php //echo $v['unit_s_price']; 
+                                                                ?>
                                                             </div>
                                                             <div class="price" style="color: #fff;">
-                                                                (<?= $v['ucv_name'] ?><?php echo $v['unit']; ?>)</div>
+                                                                (<?php // $v['ucv_name'] 
+                                                                    ?><?php //echo $v['unit']; 
+                                                                        ?>)</div>
                                                         </div>
                                                     </div>
-                                                </div>
+                                                </div> -->
                                     <?php
-                                            }
-                                        }
-                                        // }
-                                    } ?>
+                                    //         }
+                                    //     }
+                                    //     // }
+                                    // }
+                                    ?>
 
                                 </div>
                             </div>
@@ -299,9 +308,11 @@ if (!isset($_SESSION['store_id'])) {
                                         <div class="col-12" style="text-align: center;">
                                             <span style="font-size: 10px;"><?= $currentDate ?> <?= $currentTime ?></span> <br>
 
-                                            <span><span class="fw-bolder" style="font-size: 10px;"><?= $user_name ?> NO -
+                                            <span>
+                                                <span class="fw-bolder" style="font-size: 10px;"><?= $user_name ?> NO -
                                                 </span> <span class="invoiceNumber"
-                                                    id="invoiceNumber"><?= $invoiceNumber ?></span></span>
+                                                    id="invoiceNumber"><?= $invoiceNumber ?></span>
+                                            </span>
                                         </div>
                                     </div>
                                 </div>
