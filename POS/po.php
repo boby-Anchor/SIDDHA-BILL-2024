dfa277dc9a8c50aa4c8258722b44b3069ceaa24d
Pavan-De-Silva <pavanmanoj24@gmail.com>
2026-02-12 23:22:49 +0530

    remove unnecessary code

--- POS/po.php
+++ POS/po.php
@@ -6,6 +6,7 @@ if (!isset($_SESSION['store_id'])) {
 } else {
     $userData = $_SESSION['store_id'][0];
     $shop_id = $userData['shop_id'];
+    $user_name = $userData['name'];
     include('config/db.php');
 }
 // include('actions/cart-pos.php');
@@ -166,59 +167,6 @@ if (!isset($_SESSION['store_id'])) {
                                 <input type="search" class="form-control mt-2" name="search21" id="search21"
                                     onkeyup="searchProducts();" placeholder="Search...">
                                 <div class="row" id="productGrid" class="productGrid">
-                                    <?php
-                                    // if (isset($_SESSION['store_id'])) {
-
-                                    //     $userLoginData = $_SESSION['store_id'];
-
-                                    //     // foreach ($userLoginData as $userData) {
-                                    //     //     $shop_id = $userData['shop_id'];
-
-                                    //     $cm = runQuery("SELECT stock2.*, p_brand.name AS bName, p_medicine.code AS code, p_medicine.name AS name,
-                                    //         medicine_unit.unit AS unit , unit_category_variation.ucv_name
-                                    //         FROM stock2
-                                    //         INNER JOIN p_medicine ON p_medicine.code = stock2.stock_item_code
-                                    //         INNER JOIN p_brand ON p_brand.id = p_medicine.brand
-                                    //         INNER JOIN medicine_unit ON medicine_unit.id = p_medicine.medicine_unit_id
-                                    //         INNER JOIN unit_category_variation ON unit_category_variation.ucv_id = p_medicine.unit_variation
-                                    //         WHERE stock2.stock_shop_id = '$shop_id' AND stock2.stock_item_qty > 0 ORDER BY p_medicine.name ASC");
-
-                                    //     if (!empty($cm)) {
-                                    //         foreach ($cm as $v) {
-                                    ?>
-                                    <!-- <div class="col-md-4 col-sm-6 mt-3" onclick="getBarcode2('<?php // $v['code']; 
-                                                                                                    ?>')">
-                                                    <div class="product-grid h-100">
-                                                        <div class="product-content">
-                                                            <div class="name" style="color: #fff;"><?php // echo $v['name']; 
-                                                                                                    ?> <br>
-                                                                <?php // $v['code']; 
-                                                                ?></div>
-                                                            <div class="name" style="color: #f67019;"><?php // echo $v['bName']; 
-                                                                                                        ?>
-                                                            </div>
-                                                            <div class="price" style="color: #3dce12;">I:- RS
-                                                                <?php //echo $v['item_s_price']; 
-                                                                ?>
-                                                            </div>
-                                                            <div class="price" style="color: #d8f13b;">U:- RS
-                                                                <?php //echo $v['unit_s_price']; 
-                                                                ?>
-                                                            </div>
-                                                            <div class="price" style="color: #fff;">
-                                                                (<?php // $v['ucv_name'] 
-                                                                    ?><?php //echo $v['unit']; 
-                                                                        ?>)</div>
-                                                        </div>
-                                                    </div>
-                                                </div> -->
-                                    <?php
-                                    //         }
-                                    //     }
-                                    //     // }
-                                    // }
-                                    ?>
-
                                 </div>
                             </div>
 
@@ -248,104 +196,84 @@ if (!isset($_SESSION['store_id'])) {
         <div id="invoice-POS" class="d-none">
 
             <?php
-            if (isset($_SESSION['store_id'])) {
+            $currentDate = date("Y-m-d");
+            $currentTime = date("H:i:s");
 
-                $userLoginData = $_SESSION['store_id'];
-
-                $currentDate = date("Y-m-d");
-                $currentTime = date("H:i:s");
-
-                foreach ($userLoginData as $userData) {
-                    $shop_id = $userData['shop_id'];
-                    $user_name = $userData['name'];
-
-                    $invoiceId_rs = $conn->query("SELECT `AUTO_INCREMENT` FROM information_schema.tables WHERE table_schema = '$db' AND table_name = 'poinvoices'");
-                    $invoiceId_row = $invoiceId_rs->fetch_assoc();
-                    $invoiceId = $invoiceId_row['AUTO_INCREMENT'];
-                    $invoiceNumber = "000" . $userId . $shop_id . $invoiceId;
-
-                    $bill_data_rs = $conn->query("SELECT * FROM `customize_bills` WHERE `customize_bill_shop-id` = '$shop_id'");
-                    $bill_data = $bill_data_rs->fetch_assoc();
-
+            $bill_data_rs = $conn->query("SELECT * FROM `customize_bills` WHERE `customize_bill_shop-id` = '$shop_id'");
+            $bill_data = $bill_data_rs->fetch_assoc();
             ?>
-                    <div class="d-flex justify-content-center">
-                        <div class="col-12 p-2" style="width:<?= $bill_data['print_paper_size'] ?>mm ; background: whitesmoke;">
-                            <div class="row gap-1">
-                                <table>
-                                    <tr>
-                                        <td colspan="3">
-                                            <div class="col-12 d-flex justify-content-center p-2">
-                                                <div class="billpreviewlogo<?= $bill_data['print_paper_size'] ?>"
-                                                    style="background-image:url('<?= $bill_data['customize_bills_logo'] ?>');">
-                                                </div>
-                                            </div>
-                                        </td>
-                                    </tr>
+            <div class="d-flex justify-content-center">
+                <div class="col-12 p-2" style="width:<?= $bill_data['print_paper_size'] ?>mm ; background: whitesmoke;">
+                    <div class="row gap-1">
+                        <table>
+                            <tr>
+                                <td colspan="3">
+                                    <div class="col-12 d-flex justify-content-center p-2">
+                                        <div class="billpreviewlogo<?= $bill_data['print_paper_size'] ?>"
+                                            style="background-image:url('<?= $bill_data['customize_bills_logo'] ?>');">
+                                        </div>
+                                    </div>
+                                </td>
+                            </tr>
 
-                                    <tr>
-                                        <td>
-                                            <div class="col-12 d-flex justify-content-center">
-                                                <label class="contactNumber"
-                                                    id="contactNumberPreview"><?= $bill_data['customize_bills_mobile'] ?></label>
-                                            </div>
-                                            <div class="col-12 d-flex justify-content-center center">
-                                                <center>
-                                                    <label id="addresspreview"
-                                                        class="address<?= $bill_data['print_paper_size'] ?>"><?= $bill_data['customize_bills_address'] ?>
-                                                    </label>
-                                                </center>
-                                            </div>
-                                        </td>
-                                    </tr>
-                                </table>
+                            <tr>
+                                <td>
+                                    <div class="col-12 d-flex justify-content-center">
+                                        <label class="contactNumber"
+                                            id="contactNumberPreview"><?= $bill_data['customize_bills_mobile'] ?></label>
+                                    </div>
+                                    <div class="col-12 d-flex justify-content-center center">
+                                        <center>
+                                            <label id="addresspreview"
+                                                class="address<?= $bill_data['print_paper_size'] ?>"><?= $bill_data['customize_bills_address'] ?>
+                                            </label>
+                                        </center>
+                                    </div>
+                                </td>
+                            </tr>
+                        </table>
 
-                                <div class="col-12">
+                        <div class="col-12">
+                            <div class="row">
+                                <div class="col-12" style="text-align: center;">
+                                    <span style="font-size: 10px;"><?= $currentDate ?> <?= $currentTime ?></span>
+                                    <br>
+
+                                    <span>
+                                        <span class="fw-bolder" style="font-size: 10px;"><?= $user_name ?>
+                                            <br />
+                                        </span>
+                                        To-
+                                        <span id="po_shop_on_bill">
+                                        </span>
+                                        <span class="invoiceNumber" id="invoiceNumber"> </span>
+                                    </span>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="col-12" style="border-bottom: #0e0e0e 0.2rem solid;"></div>
+                    </div>
+                    <div class="printInvoiceData" id="printInvoiceData"> </div>
+                    <table>
+                        <tr style="font-weight: 600;">
+                            <td>
+                                <div class="col-12 pt-2">
                                     <div class="row">
-                                        <div class="col-12" style="text-align: center;">
-                                            <span style="font-size: 10px;"><?= $currentDate ?> <?= $currentTime ?></span>
-                                            <br>
-
-                                            <span>
-                                                <span class="fw-bolder" style="font-size: 10px;"><?= $user_name ?>
-                                                    <br />
-                                                </span>
-                                                To-
-                                                <span id="po_shop_on_bill">
-                                                </span>
-                                                <span class="invoiceNumber"
-                                                    id="invoiceNumber"><?= $invoiceNumber ?>
-                                                </span>
-                                            </span>
+                                        <div class="col-12 d-flex justify-content-center text-center">
+                                            <span id="billnotepreview"
+                                                style="font-size:9px;"><?= $bill_data['bill_note'] ?></span>
+                                        </div>
+                                        <div class="col-12 d-flex justify-content-center">
+                                            <span>Thank You !</span>
                                         </div>
                                     </div>
                                 </div>
-
-                                <div class="col-12" style="border-bottom: #0e0e0e 0.2rem solid;"></div>
-                            </div>
-                            <div class="printInvoiceData" id="printInvoiceData"> </div>
-                            <table>
-                                <tr style="font-weight: 600;">
-                                    <td>
-                                        <div class="col-12 pt-2">
-                                            <div class="row">
-                                                <div class="col-12 d-flex justify-content-center text-center">
-                                                    <span id="billnotepreview"
-                                                        style="font-size:9px;"><?= $bill_data['bill_note'] ?></span>
-                                                </div>
-                                                <div class="col-12 d-flex justify-content-center">
-                                                    <span>Thank You !</span>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </td>
-                                </tr>
-                            </table>
-                        </div>
-                    </div>
-            <?php
-                }
-            }
-            ?>
+                            </td>
+                        </tr>
+                    </table>
+                </div>
+            </div>
         </div>
 
         <!-- ========================================== -->
