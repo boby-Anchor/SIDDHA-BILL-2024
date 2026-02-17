14d1c3c9dccfa477dd37a2d0bda0a807dd178bae
Pavan-De-Silva <pavanmanoj24@gmail.com>
2026-02-17 20:30:26 +0530

    suppliers status updating done

--- POS/manage-supplier.php
+++ POS/manage-supplier.php
@@ -121,6 +121,46 @@ if (!isset($_SESSION['store_id'])) {
         theme: "bootstrap4",
       });
     });
+
+    function updateStatus(id, status) {
+      $.ajax({
+        url: "actions/global/statusUpdate.php/update",
+        type: "POST",
+        data: {
+          id: id,
+          table: "p_supplier",
+          status: status,
+        },
+        success: function(response) {
+          const result = JSON.parse(response);
+
+          switch (result.status) {
+            case 'success':
+              SuccessMessageDisplay(result.message);
+              setTimeout(() => {
+                location.reload();
+              }, 5000);
+              break;
+            case 'session_expired':
+              handleExpiredSession(result.message);
+              return;
+              break;
+            case 'error':
+              ErrorMessageDisplay(result.message);
+              return;
+              break;
+
+            default:
+              ErrorMessageDisplay("An unknown error occurred.");
+              return;
+              break;
+          };
+        },
+        error: function(xhr, status, error) {
+          ErrorMessageDisplay("Something went wrong! Check connection.");
+        },
+      });
+    }
   </script>
 
 </body>
